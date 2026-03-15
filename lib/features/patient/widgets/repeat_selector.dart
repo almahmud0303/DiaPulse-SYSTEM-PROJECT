f2d01bd515d67a93eb08e1be428f4652034972b8
Nirob2107092 <perfeccnirob@gmail.com>
2026-03-15 16:46:30 +0600

    added reminder widget ui and screens

---
+++ lib/features/patient/widgets/repeat_selector.dart
@@ -0,0 +1,42 @@
+import 'package:dia_plus/models/reminder_repeat_mode.dart';
+import 'package:flutter/material.dart';
+
+class RepeatSelector extends StatelessWidget {
+  const RepeatSelector({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    this.labelText = 'Repeat',
+    this.validator,
+    this.isDense = false,
+  });
+
+  final ReminderRepeatMode value;
+  final ValueChanged<ReminderRepeatMode> onChanged;
+  final String labelText;
+  final String? Function(ReminderRepeatMode?)? validator;
+  final bool isDense;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<ReminderRepeatMode>(
+      value: value,
+      isDense: isDense,
+      decoration: InputDecoration(labelText: labelText),
+      items: ReminderRepeatMode.values
+          .map(
+            (mode) => DropdownMenuItem<ReminderRepeatMode>(
+              value: mode,
+              child: Text(mode.label),
+            ),
+          )
+          .toList(),
+      validator: validator,
+      onChanged: (next) {
+        if (next != null) {
+          onChanged(next);
+        }
+      },
+    );
+  }
+}
